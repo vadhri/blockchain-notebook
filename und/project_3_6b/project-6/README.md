76b38c03ac0a05c0b381f33538611a4c38473d0d
Venkat <vadhrivenkata.ratnam@nagra.com>
2022-07-08 18:45:35 +0530

    Project init

--- und/project_3_6b/project-6/README.md
+++ und/project_3_6b/project-6/README.md
@@ -3,3 +3,14 @@ Ganache v7.2.0
 Solidity - 0.8.15 (solc-js)
 Node v18.2.0
 Web3.js v1.5.3
+
+Contract address on public test network ( Goerli )      
+https://goerli.etherscan.io/address/0x5f253697d0464e5d33ecb2c9aa984e01eb17a505#codes
+
+Transactions. 
+============
+Processed - 0x778a30d3de29a5889f87ef681aec7b12a39b6cfdbd4ca4348da6d24d3f44afce
+Packed - 0x231ab9453785203ece91ea6469d6155fbf6533fa3278766602e71f707b3a481e
+ForSale - 0x0fff8712d74276f6296d6e64c5ac1a38dc53c85efc647d4c9faec95570933ae1
+Sold - 0x4de0c98c31ae1277a89c98c878d089b65ddc183ba1552e491d89a2792aa23175
+Shipped - 0x75dc50ec9ada2c456ced72e0a8ba58b1ba4ed236c8f4112cc260ad489bee0dac
--- und/project_3_6b/project-6/truffle.js
+++ und/project_3_6b/project-6/truffle.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   compilers: {
     solc: {
@@ -10,6 +12,21 @@ module.exports = {
       port: 7545,
       network_id: "*", // Match any network id
       websockets: true
-    }
+    },  
+    goerli: {
+      provider: () =>
+        new HDWalletProvider({
+          privateKeys: ["******"],
+          providerOrUrl:
+            'wss://goerli.infura.io/ws/v3/******',
+          addressIndex: 0
+        }),      
+      network_id: 5,       
+      gas: 5500000,         
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false,     // Skip dry run before migrations? (default: false for public nets )
+      from: "******"
+    },
   }
 };
